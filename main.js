a0002ea72f4ca800adf557cb2d6f62b935aacf6d
Shubham <firstsj2002@gmail.com>
2024-09-27 00:15:00 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+function showdiv() {
+    document.getElementById("mesha").style.visibility = "visible";
+  }
+  setTimeout("showdiv()", 0);
+  
+  
+  
+  function hidediv() {
+    document.getElementById("mesha").style.visibility = "hidden";
+  }
+  setTimeout("hidediv()", 10900);
+  
+  
+  
+  function showdivml() {
+    document.getElementById("meshlog").style.visibility = "visible";
+  }
+  setTimeout("showdivml()", 0);
+  
+  
+  
+  function hidedivml() {
+    document.getElementById("meshlog").style.visibility = "hidden";
+  }
+  setTimeout("hidedivml()", 10900);
+  
+  
+  
+
+
+
+
+  
+  function showdiv331() {
+    document.getElementById("particles-js").style.visibility = "visible";
+  }
+  setTimeout("showdiv331()", 10900);
+    
+
+
+
+  function showdiv332() {
+    document.getElementById("mainlink01").style.visibility = "visible";
+  }
+  setTimeout("showdiv332()", 10900);
+
+
+
+
+    
+  function showdiv333() {
+    document.getElementById("particle-slider").style.height = "0vh";
+  }
+  setTimeout("showdiv333()", 10900);
+
+
+    
---
+++ meshalogo.js
@@ -0,0 +1,281 @@
+
+//dot 0
+function showdiv() { document.getElementById("step01").style.visibility = "visible";
+}// lines 1to12 //line 01 
+setTimeout("showdiv()", 11000); function showdiv0101() { document.getElementById("step0201").style.visibility = "visible";
+}
+setTimeout("showdiv0101()", 11050); function showdiv0102() { document.getElementById("step0202").style.visibility = "visible";
+}
+setTimeout("showdiv0102()", 11100); function showdiv0103() { document.getElementById("step0203").style.visibility = "visible";
+}
+setTimeout("showdiv0103()", 11150); function showdiv0104() { document.getElementById("step0204").style.visibility = "visible";
+}
+setTimeout("showdiv0104()", 11200); function showdiv0105() { document.getElementById("step0205").style.visibility = "visible";
+}
+setTimeout("showdiv0105()", 11250); function showdiv0106() { document.getElementById("step0206").style.visibility = "visible";
+}
+setTimeout("showdiv0106()", 11300); function showdiv0107() { document.getElementById("step0207").style.visibility = "visible";
+}
+setTimeout("showdiv0107()", 11350); function showdiv0108() { document.getElementById("step0208").style.visibility = "visible";
+}
+setTimeout("showdiv0108()", 11400); function showdiv0109() { document.getElementById("step0209").style.visibility = "visible";
+}
+setTimeout("showdiv0109()", 11450); function showdiv0110() { document.getElementById("step0210").style.visibility = "visible";
+}//line 02 
+setTimeout("showdiv0110()", 11500); function showdiv0201() { document.getElementById("step0311").style.visibility = "visible";
+}
+setTimeout("showdiv0201()", 11550); function showdiv0202() { document.getElementById("step0312").style.visibility = "visible";
+}
+setTimeout("showdiv0202()", 11600); function showdiv0203() { document.getElementById("step0313").style.visibility = "visible";
+}
+setTimeout("showdiv0203()", 11650); function showdiv0204() { document.getElementById("step0314").style.visibility = "visible";
+}
+setTimeout("showdiv0204()", 11700); function showdiv0205() { document.getElementById("step0315").style.visibility = "visible";
+}
+setTimeout("showdiv0205()", 11750); function showdiv0206() { document.getElementById("step0316").style.visibility = "visible";
+}
+setTimeout("showdiv0206()", 11800); function showdiv0207() { document.getElementById("step0317").style.visibility = "visible";
+}//line 03 
+setTimeout("showdiv0207()", 11850); function showdiv0301() { document.getElementById("step0418").style.visibility = "visible";
+}
+setTimeout("showdiv0301()", 11900); function showdiv0302() { document.getElementById("step0419").style.visibility = "visible";
+}
+setTimeout("showdiv0302()", 12000); function showdiv0303() { document.getElementById("step0420").style.visibility = "visible";
+}
+setTimeout("showdiv0303()", 12050); function showdiv0304() { document.getElementById("step0421").style.visibility = "visible";
+}
+setTimeout("showdiv0304()", 12100); function showdiv0305() { document.getElementById("step0422").style.visibility = "visible";
+}
+setTimeout("showdiv0305()", 12150); function showdiv0306() { document.getElementById("step0423").style.visibility = "visible";
+}
+setTimeout("showdiv0306()", 12200); function showdiv0307() { document.getElementById("step0424").style.visibility = "visible";
+}//line 04 
+setTimeout("showdiv0307()", 12250); function showdiv0401() { document.getElementById("step0525").style.visibility = "visible";
+}
+setTimeout("showdiv0401()", 12300); function showdiv0402() { document.getElementById("step0526").style.visibility = "visible";
+}
+setTimeout("showdiv0402()", 12350); function showdiv0403() { document.getElementById("step0527").style.visibility = "visible";
+}
+setTimeout("showdiv0403()", 12400); function showdiv0404() { document.getElementById("step0528").style.visibility = "visible";
+}
+setTimeout("showdiv0404()", 12450); function showdiv0405() { document.getElementById("step0529").style.visibility = "visible";
+}
+setTimeout("showdiv0405()", 12500); function showdiv0406() { document.getElementById("step0530").style.visibility = "visible";
+}
+setTimeout("showdiv0406()", 12550); function showdiv0407() { document.getElementById("step0531").style.visibility = "visible";
+}//line 05 
+setTimeout("showdiv0407()", 12600); function showdiv0501() { document.getElementById("step0632").style.visibility = "visible";
+}
+setTimeout("showdiv0501()", 12650); function showdiv0502() { document.getElementById("step0633").style.visibility = "visible";
+}
+setTimeout("showdiv0502()", 12700); function showdiv0503() { document.getElementById("step0634").style.visibility = "visible";
+}
+setTimeout("showdiv0503()", 12750); function showdiv0504() { document.getElementById("step0635").style.visibility = "visible";
+}
+setTimeout("showdiv0504()", 12800); function showdiv0505() { document.getElementById("step0636").style.visibility = "visible";
+}
+setTimeout("showdiv0505()", 12850); function showdiv0506() { document.getElementById("step0637").style.visibility = "visible";
+}
+setTimeout("showdiv0506()", 12900); function showdiv0507() { document.getElementById("step0638").style.visibility = "visible";
+}//line 06 
+setTimeout("showdiv0507()", 12950); function showdiv0601() { document.getElementById("step0739").style.visibility = "visible";
+}
+setTimeout("showdiv0601()", 13000); function showdiv0602() { document.getElementById("step0740").style.visibility = "visible";
+}
+setTimeout("showdiv0602()", 13050); function showdiv0603() { document.getElementById("step0741").style.visibility = "visible";
+}
+setTimeout("showdiv0603()", 13100); function showdiv0604() { document.getElementById("step0742").style.visibility = "visible";
+}
+setTimeout("showdiv0604()", 13150); function showdiv0606() { document.getElementById("step0743").style.visibility = "visible";
+}
+setTimeout("showdiv0606()", 13200); function showdiv0607() { document.getElementById("step0744").style.visibility = "visible";
+}//line 07 
+setTimeout("showdiv0607()", 13250); function showdiv0701() { document.getElementById("step0845").style.visibility = "visible";
+}
+setTimeout("showdiv0701()", 13300); function showdiv0702() { document.getElementById("step0846").style.visibility = "visible";
+}
+setTimeout("showdiv0702()", 13350); function showdiv0703() { document.getElementById("step0847").style.visibility = "visible";
+}
+setTimeout("showdiv0703()", 13400); function showdiv0704() { document.getElementById("step0848").style.visibility = "visible";
+}
+setTimeout("showdiv0704()", 13450); function showdiv0705() { document.getElementById("step0849").style.visibility = "visible";
+}
+setTimeout("showdiv0705()", 13500); function showdiv0706() { document.getElementById("step0850").style.visibility = "visible";
+}//line 08 
+setTimeout("showdiv0706()", 13550); function showdiv0801() { document.getElementById("step0951").style.visibility = "visible";
+}
+setTimeout("showdiv0801()", 13600); function showdiv0802() { document.getElementById("step0952").style.visibility = "visible";
+}
+setTimeout("showdiv0802()", 13650); function showdiv0803() { document.getElementById("step0953").style.visibility = "visible";
+}
+setTimeout("showdiv0803()", 13700); function showdiv0804() { document.getElementById("step0954").style.visibility = "visible";
+}
+setTimeout("showdiv0804()", 13750); function showdiv0805() { document.getElementById("step0955").style.visibility = "visible";
+}//line 09
+setTimeout("showdiv0805()", 13800); function showdiv0901() { document.getElementById("step1056").style.visibility = "visible";
+}
+setTimeout("showdiv0901()", 13850); function showdiv0902() { document.getElementById("step1057").style.visibility = "visible";
+}
+setTimeout("showdiv0902()", 13900); function showdiv0903() { document.getElementById("step1058").style.visibility = "visible";
+}
+setTimeout("showdiv0903()", 13950); function showdiv0904() { document.getElementById("step1059").style.visibility = "visible";
+}
+setTimeout("showdiv0904()", 14000); function showdiv0905() { document.getElementById("step1060").style.visibility = "visible";
+}
+setTimeout("showdiv0905()", 14050); function showdiv0906() { document.getElementById("step1061").style.visibility = "visible";
+}//end of
+setTimeout("showdiv0906()", 14150);  function showdiv1001() { document.getElementById("step1162").style.visibility = "visible";
+}
+setTimeout("showdiv1001()", 14250); function showdiv1002() { document.getElementById("step1163").style.visibility = "visible";
+}
+setTimeout("showdiv1002()", 14350); function showdiv1003() { document.getElementById("step1164").style.visibility = "visible";
+}
+setTimeout("showdiv1003()", 14450); function showdiv1004() { document.getElementById("step1165").style.visibility = "visible";
+} // i forgot to write step66 sorry .lol
+setTimeout("showdiv1004()", 14550); function showdiv1005() { document.getElementById("step1167").style.visibility = "visible";
+}
+setTimeout("showdiv1005()", 14650); function showdiv1006() { document.getElementById("step1168").style.visibility = "visible";
+}
+setTimeout("showdiv1006()", 14750); function showdiv1007() { document.getElementById("step1169").style.visibility = "visible";
+}
+setTimeout("showdiv1007()", 14850); function showdiv1101() { document.getElementById("step1270").style.visibility = "visible";
+}
+setTimeout("showdiv1101()", 14950); function showdiv1102() { document.getElementById("step1271").style.visibility = "visible";
+}
+setTimeout("showdiv1102()", 15050); function showdiv1103() { document.getElementById("step1272").style.visibility = "visible";
+}
+setTimeout("showdiv1103()", 15150); function showdiv1104() { document.getElementById("step1273").style.visibility = "visible";
+}
+setTimeout("showdiv1104()", 15250); function showdiv1105() { document.getElementById("step1274").style.visibility = "visible";
+}
+setTimeout("showdiv1105()", 15350); function showdiv1201() { document.getElementById("step1375").style.visibility = "visible";
+}
+setTimeout("showdiv1201()", 15450); function showdiv1202() { document.getElementById("step1376").style.visibility = "visible";
+}
+setTimeout("showdiv1202()", 15550); function showdiv1203() { document.getElementById("step1377").style.visibility = "visible";
+}
+setTimeout("showdiv1203()", 15650); function showdiv1204() { document.getElementById("step1378").style.visibility = "visible";
+}
+setTimeout("showdiv1204()", 15750); function showdiv1205() { document.getElementById("step1379").style.visibility = "visible";
+}
+setTimeout("showdiv1205()", 15850); function showdiv1206() { document.getElementById("step1380").style.visibility = "visible";
+}
+setTimeout("showdiv1206()", 15900); function showdiv13() { document.getElementById("step14").style.visibility = "visible";
+}
+setTimeout("showdiv13()", 15950); function showdiv14() { document.getElementById("step15").style.visibility = "visible";
+}
+setTimeout("showdiv14()", 16000); function showdiv15() { document.getElementById("step16").style.visibility = "visible";
+}
+setTimeout("showdiv15()", 16050); function showdiv16() { document.getElementById("step17").style.visibility = "visible";
+}
+setTimeout("showdiv16()", 16100); function showdiv17() { document.getElementById("step18").style.visibility = "visible";
+}
+setTimeout("showdiv17()", 16120); function showdiv18() { document.getElementById("step19").style.visibility = "visible";
+}
+setTimeout("showdiv18()", 16140); function showdiv19() { document.getElementById("step20").style.visibility = "visible";
+}
+setTimeout("showdiv19()", 16160); function showdiv20() { document.getElementById("step21").style.visibility = "visible";
+}
+setTimeout("showdiv20()", 16180); function showdiv21() { document.getElementById("step22").style.visibility = "visible";
+}
+setTimeout("showdiv21()", 16200); function showdiv22() { document.getElementById("step23").style.visibility = "visible";
+}
+setTimeout("showdiv22()", 16220); function showdiv23() { document.getElementById("step24").style.visibility = "visible";
+}
+setTimeout("showdiv23()", 16240); function showdiv24() { document.getElementById("step25").style.visibility = "visible";
+}
+setTimeout("showdiv24()", 16260); function showdiv25() { document.getElementById("step26").style.visibility = "visible";
+}
+setTimeout("showdiv25()", 16280); function showdiv26() { document.getElementById("step27").style.visibility = "visible";
+}
+setTimeout("showdiv26()", 16300); function showdiv27() { document.getElementById("step28").style.visibility = "visible";
+}
+setTimeout("showdiv27()", 16320); function showdiv28() { document.getElementById("step29").style.visibility = "visible";
+}
+setTimeout("showdiv28()", 16340); function showdiv29() { document.getElementById("step30").style.visibility = "visible";
+}
+setTimeout("showdiv29()", 16360); function showdiv30() { document.getElementById("step31").style.visibility = "visible";
+}
+setTimeout("showdiv30()", 16380); function showdiv31() { document.getElementById("step32").style.visibility = "visible";
+}
+setTimeout("showdiv31()", 16400); function showdiv32() { document.getElementById("step33").style.visibility = "visible";
+}
+setTimeout("showdiv32()", 16420); function showdiv33() { document.getElementById("step34").style.visibility = "visible";
+}
+setTimeout("showdiv33()", 16440); function showdiv34() { document.getElementById("step35").style.visibility = "visible";
+}
+setTimeout("showdiv34()", 16540); function showdiv35() { document.getElementById("step36").style.visibility = "visible";
+}
+setTimeout("showdiv35()", 16640); function showdiv36() { document.getElementById("step37").style.visibility = "visible";
+}
+setTimeout("showdiv36()", 16740); function showdiv37() { document.getElementById("step38").style.visibility = "visible";
+}
+setTimeout("showdiv37()", 16840); function showdiv38() { document.getElementById("step39").style.visibility = "visible";
+}
+setTimeout("showdiv38()", 16940); function showdiv39() { document.getElementById("step40").style.visibility = "visible";
+}
+//get started
+setTimeout("showdiv39()", 17040); function showdiv40() { document.getElementById("place").style.visibility = "visible";
+}
+//wand
+setTimeout("showdiv40()", 17100); function showdiv41() { document.getElementById("step42").style.visibility = "visible";
+}
+setTimeout("showdiv41()", 17150); function showdiv42() { document.getElementById("step43").style.visibility = "visible";
+}
+setTimeout("showdiv42()", 17200); function showdiv43() { document.getElementById("step44").style.visibility = "visible";
+}
+setTimeout("showdiv43()", 17250); function showdiv44() { document.getElementById("step45").style.visibility = "visible";
+}
+setTimeout("showdiv44()", 17300); function showdiv45() { document.getElementById("step47").style.visibility = "visible";
+}
+setTimeout("showdiv45()", 17350); function showdiv46() { document.getElementById("step47").style.visibility = "visible";
+}
+setTimeout("showdiv46()", 17400); function showdiv47() { document.getElementById("step48").style.visibility = "visible";
+}
+setTimeout("showdiv47()", 17450); function showdiv48() { document.getElementById("step49").style.visibility = "visible";
+}
+setTimeout("showdiv48()", 17500); function showdiv49() { document.getElementById("step50").style.visibility = "visible";
+}
+setTimeout("showdiv49()", 17550); function showdiv50() { document.getElementById("step51").style.visibility = "visible";
+}
+setTimeout("showdiv50()", 17600); function showdiv51() { document.getElementById("step52").style.visibility = "visible";
+}
+setTimeout("showdiv51()", 17650); function showdiv52() { document.getElementById("step53").style.visibility = "visible";
+}
+setTimeout("showdiv52()", 17700); function showdiv53() { document.getElementById("step54").style.visibility = "visible";
+}
+setTimeout("showdiv53()", 17750); function showdiv5401() { document.getElementById("step551").style.visibility = "visible";
+}
+setTimeout("showdiv5401()", 17800); function showdiv5402() { document.getElementById("step552").style.visibility = "visible";
+}
+setTimeout("showdiv5402()", 17850); function showdiv5403() { document.getElementById("step553").style.visibility = "visible";
+}
+setTimeout("showdiv5403()", 17900); function showdiv5404() { document.getElementById("step554").style.visibility = "visible";
+}
+setTimeout("showdiv5404()", 17950); function showdiv5405() { document.getElementById("step555").style.visibility = "visible";
+}
+setTimeout("showdiv5405()", 18000); function showdiv5406() { document.getElementById("step556").style.visibility = "visible";
+}
+setTimeout("showdiv5406()", 18050); function showdiv5407() { document.getElementById("step557").style.visibility = "visible";
+}
+setTimeout("showdiv5407()", 18100); function showdiv5408() { document.getElementById("step558").style.visibility = "visible";
+}
+setTimeout("showdiv5408()", 18150); function showdiv5409() { document.getElementById("step559").style.visibility = "visible";
+}
+setTimeout("showdiv5409()", 18200); function showdiv5410() { document.getElementById("step5510").style.visibility = "visible";
+}
+setTimeout("showdiv5410()", 18250); function showdiv5411() { document.getElementById("step5511").style.visibility = "visible";
+}
+setTimeout("showdiv5411()", 18300); function showdiv5412() { document.getElementById("step5512").style.visibility = "visible";
+}
+setTimeout("showdiv5412()", 18350); function showdiv5413() { document.getElementById("step5513").style.visibility = "visible";
+}
+setTimeout("showdiv5413()", 18400); function showdiv5414() { document.getElementById("step5514").style.visibility = "visible";
+}
+setTimeout("showdiv5414()", 18450); function showdiv5415() { document.getElementById("step5515").style.visibility = "visible";
+}
+setTimeout("showdiv5415()", 18500); function showdiv5416() { document.getElementById("step5516").style.visibility = "visible";
+}
+setTimeout("showdiv5416()", 18550); function showdiv5417() { document.getElementById("step5517").style.visibility = "visible";
+}
+setTimeout("showdiv5417()", 18600); function showdiv5418() { document.getElementById("step5518").style.visibility = "visible";}
